226d871c632dae00076712b0601f897309720e0b
ajaa1 <ajaa1@MARIBELLENOVO>
2023-05-21 17:21:10 -0400

    Agregar archivos de proyecto.

---
+++ Chat online/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+[assembly:OwinStartupAttribute(typeof(Chat_online.Startup))]
+
+namespace Chat_online
+{
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
